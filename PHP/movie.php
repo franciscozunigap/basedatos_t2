54351f13cc428e499e65782f57e7ad5722a4b0e2
Francisco Zúñiga <francisco.zunigap@usm.cl>
2022-11-19 23:49:01 -0300

    Update movie.php

---
+++ PHP/movie.php
@@ -0,0 +1,36 @@
+<?php
+
+require "header.php";
+
+
+
+?>
+
+<body>
+    <title>
+        Blockbusm
+    </title>
+
+    <div class="movie">
+        <h1>Reseñas</h1>
+         
+        <nav class="photo moviek">
+            <a class="photo movie" href="/PHP/">
+            <img src="/../IMG/logo.png"  class="logo" > //pedir foto pelicula a BD
+        </a>
+
+        <h2> nombre </h2> // Pedir nombre a BD
+
+        
+    
+
+        </div>
+
+
+    </div>
+
+</body>
+
+
+
+</body>
